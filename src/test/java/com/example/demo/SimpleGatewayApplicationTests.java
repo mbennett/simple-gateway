26f6901ab51782495035a4f9d5c1c9bc17acc266
Matt Bennett <matt.bennett@vml.com>
2018-03-30 13:02:50 -0500

    I think I got it all figured out.

--- src/test/java/com/example/demo/SimpleGatewayApplicationTests.java
+++ src/test/java/com/example/demo/SimpleGatewayApplicationTests.java
@@ -3,12 +3,12 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles(profiles = "test")
+@TestPropertySource(properties = { "eureka.client.enabled=false" })
 public class SimpleGatewayApplicationTests {
 
     @Test
